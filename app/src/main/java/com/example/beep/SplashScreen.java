9b40632806776cd6c062183f848b8859c8f7d12b
owais <owais.khanabc12@gmail.com>
2022-05-24 23:03:59 +0530

    added basic activities and splash screen

---
+++ app/src/main/java/com/example/beep/SplashScreen.java
@@ -0,0 +1,38 @@
+package com.example.beep;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.VideoView;
+
+public class SplashScreen extends AppCompatActivity {
+    VideoView vi;
+    Handler handler;
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+        vi = findViewById(R.id.beep);
+        vi.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.beep_splashscreen);
+        vi.start();
+        handler = new Handler();
+        handler.postDelayed(() ->
+                {
+
+                    Intent intent = new Intent(SplashScreen.this, Home.class);
+                    startActivity(intent);
+                    finish();
+                },3000);
+
+    }
+    }
